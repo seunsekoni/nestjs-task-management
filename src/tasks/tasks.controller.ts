b265aad7ba26e10d663a3211780879b618dd0214
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2021-09-12 12:35:43 +0100

    feat: Added guards to the task controller. Only auth users can view all tasks.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,7 +7,9 @@ import {
 	Patch,
 	Post,
 	Query,
+	UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -15,6 +17,7 @@ import { Task } from './tasks.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard)
 export class TasksController {
 	constructor(private tasksService: TasksService) {}
 
